077a12934946d66499d3428c61ae63a9ad71dce7
Fagner Sales <fs.fagnersales@gmail.com>
2023-05-22 11:14:56 -0300

    add tags page

---
+++ src/app/tags/[tag]/page.tsx
@@ -0,0 +1,25 @@
+import { allPosts } from "contentlayer/generated";
+import { NextPage } from "next";
+import { tagSlug } from "~/app/utils/slugs";
+import { Cards } from "~/components/cards";
+
+export const generateStaticParams = () =>
+  allPosts.map((post) => post.tags.map((tag) => ({ tag: tagSlug(tag) }))).flat();
+
+const TagPage: NextPage<{ params: { tag: string } }> = ({ params }) => {
+  const posts = allPosts.filter((post) =>
+    post.tags.some((tag) => tag.toLowerCase() === params.tag.toLowerCase())
+  );
+
+  return (
+    <section className="m-8 grid gap-4 lg:m-auto lg:my-8 lg:max-w-3xl">
+      <h1 className="border-b-2 border-b-gray-700 pb-4 text-3xl font-bold text-gray-200">
+        {`#${params.tag.toUpperCase()}`}
+      </h1>
+      <Cards posts={posts} />
+      {/* <InitialPageButton text="Página Inicial" /> */}
+    </section>
+  );
+};
+
+export default TagPage;
--- src/app/utils/slugs.ts
+++ src/app/utils/slugs.ts
@@ -4,6 +4,6 @@ export const tagSlug = (tag: string) => tag.toLowerCase();
 export const authorSlug = (author: string) =>
   author.toLowerCase().replace(" ", "");
 
-export const tagSlugUrl = (tag: string) => `/tag/${tagSlug(tag)}/`;
+export const tagSlugUrl = (tag: string) => `/tags/${tagSlug(tag)}/`;
 export const authorSlugUrl = (author: string) => `/${authorSlug(author)}/`;
 export const postSlugUrl = (post: Post) => `/posts/${post.slug}`;
