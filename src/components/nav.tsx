d5c791e576993e913fcca6598b1feb2ce8ef6a7e
Fagner Sales <fs.fagnersales@gmail.com>
2023-05-22 15:52:17 -0300

    reverse style changes

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -7,22 +7,19 @@ import Image from "next/image";
 export function Nav() {
   return (
     <>
-      <header
-        id="header"
-        className="primary-header sticky top-0 grid grid-flow-col place-items-center p-8 opacity-50 transition-opacity duration-500 hover:opacity-100"
-      >
-        <Link href="/">
-          <div className="flex flex-row place-items-center gap-2">
-            <Image
-              src="logo_no_fill.svg"
-              alt="Vercel's logo"
-              width={50}
-              height={50}
-            />
-            <span className="hover:animate-pulse-slow text-2xl font-black uppercase text-[#828282]">
-              Nibo Space
-            </span>
-          </div>
+      <header className="primary-header sticky top-0 grid grid-flow-col place-items-center bg-neutral-900 p-8 opacity-50 transition-opacity duration-500 hover:opacity-100">
+      <Link href="/">
+        <div className="flex flex-row place-items-center gap-2">
+          <Image
+            src="logo_no_fill.svg"
+            alt="Vercel's logo"
+            width={50}
+            height={50}
+          />
+          <span className="hover:animate-pulse-slow text-2xl font-black uppercase text-[#828282]">
+            Nibo Space
+          </span>
+        </div>
         </Link>
       </header>
 
@@ -32,22 +29,6 @@ export function Nav() {
           top: 0;
           opacity: 1;
           box-shadow: 0 2px 1px 1px rgba(0, 0, 0, 0.5);
-        }
-
-        #header {
-          background: radial-gradient(
-            rgba(255, 255, 255, 0.1) 8%,
-            transparent 8%
-          );
-          background-position: 0% 0%;
-          background-size: 30px 30px;
-          height: 100%;
-          width: 100%;
-          transition: background-position 500ms ease;
-        }
-
-        #header:hover {
-          background-position: -10% 0%;
         }
       `}</style>
 
